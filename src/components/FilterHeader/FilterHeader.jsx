82c0d04b4561cf406a18d02f0ba30c8ba7f02ec4
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2022-01-12 00:23:11 +0530

    created inbox and filter and added local state for choosing filter

--- src/components/FilterHeader/FilterHeader.jsx
+++ src/components/FilterHeader/FilterHeader.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./FilterHeader.css";
 
-const FilterHeader = () => {
-  return <header></header>;
+const FilterHeader = ({ category, selectFilter }) => {
+  const filters = ["Unread", "Read", "Favorites"];
+
+  const handleCategorySelect = (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      selectFilter(e.target.id);
+    }
+  };
+  return (
+    <header className="header__container">
+      <p>Filter By:</p>
+      <div onClick={handleCategorySelect}>
+        {filters.map((filter, i) => (
+          <button
+            key={i}
+            id={filter}
+            className={category === filter ? "active" : ""}
+          >
+            {filter}
+          </button>
+        ))}
+      </div>
+    </header>
+  );
 };
 
 export default FilterHeader;
--- src/components/Inbox/Inbox.jsx
+++ src/components/Inbox/Inbox.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Avatar from "../Avatar/Avatar";
+import Email from "../Email/Email";
 import FilterHeader from "../FilterHeader/FilterHeader";
 import "./Inbox.css";
 
 const Inbox = () => {
+  const [filter, setFilter] = useState("Unread");
+
+  const changeFilter = function (name) {
+    setFilter(name);
+  };
+
   return (
     <div className="container">
-      <FilterHeader />
-      <body>
-        <ul></ul>
-      </body>
+      <FilterHeader category={filter} selectFilter={changeFilter} />
+      <div className="container__body">
+        <nav>
+          <ul>
+            {[...Array(10)].map((item) => (
+              <Email />
+            ))}
+          </ul>
+        </nav>
+        <section className="email__body">
+          <header>
+            <Avatar />
+            <div className="email__body__header">
+              <div className="email__body__info">
+                <h1>Lorem Ipsum</h1>
+                <p>26/02/2020 10:30am</p>
+              </div>
+              <button className="favorite">Mark as favorite</button>
+            </div>
+          </header>
+          <div className="email__body__content">
+            <p>ljsdljqwdjpqwjdpoqwdpojqwdpojqwpdj</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
