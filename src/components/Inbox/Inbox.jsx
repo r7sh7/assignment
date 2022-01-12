dc07131eeae76f16106abe4222077560fec4a3b2
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2022-01-12 11:18:52 +0530

    UI setup and local state management done

--- src/components/Inbox/Inbox.jsx
+++ src/components/Inbox/Inbox.jsx
@@ -6,9 +6,31 @@ import "./Inbox.css";
 
 const Inbox = () => {
   const [filter, setFilter] = useState("Unread");
+  const [favorites, setFavorites] = useState([]);
+  const [readEmails, setReadEmails] = useState([]);
 
   const changeFilter = function (name) {
     setFilter(name);
+  };
+
+  const displayEmailBody = function () {
+    return (
+      <section className="email__body">
+        <header>
+          <Avatar />
+          <div className="email__body__header">
+            <div className="email__body__info">
+              <h1>Lorem Ipsum</h1>
+              <p>26/02/2020 10:30am</p>
+            </div>
+            <button className="favorite">Mark as favorite</button>
+          </div>
+        </header>
+        <div className="email__body__content">
+          <p>ljsdljqwdjpqwjdpoqwdpojqwdpojqwpdj</p>
+        </div>
+      </section>
+    );
   };
 
   return (
@@ -22,21 +44,7 @@ const Inbox = () => {
             ))}
           </ul>
         </nav>
-        <section className="email__body">
-          <header>
-            <Avatar />
-            <div className="email__body__header">
-              <div className="email__body__info">
-                <h1>Lorem Ipsum</h1>
-                <p>26/02/2020 10:30am</p>
-              </div>
-              <button className="favorite">Mark as favorite</button>
-            </div>
-          </header>
-          <div className="email__body__content">
-            <p>ljsdljqwdjpqwjdpoqwdpojqwdpojqwpdj</p>
-          </div>
-        </section>
+        {displayEmailBody()}
       </div>
     </div>
   );
